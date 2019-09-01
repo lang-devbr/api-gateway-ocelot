6c3d7dea001243cb7356f40fd04d0500dc3a637e
lang <lang.codes@gmail.com>
2019-09-01 13:43:32 -0300

    Init commit

---
+++ src/ApiGateway/ApiGateway/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace ApiGateway
+{
+    public class Program
+    {
+        //public static void Main(string[] args)
+        //{
+        //    CreateWebHostBuilder(args).Build().Run();
+        //}
+
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .ConfigureAppConfiguration(ic => ic.AddJsonFile("configuration.json"))
+        //        .UseStartup<Startup>();
+
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            return WebHost.CreateDefaultBuilder(args)
+                            .UseStartup<Startup>()
+                            .ConfigureAppConfiguration((hostingContext, builder) =>
+                            {
+                                builder.AddJsonFile("configuration.json", false, true);
+                                builder.AddJsonFile("appsettings.json", false, true);
+                            })
+                            .Build();
+        }
+    }
+}
---
+++ src/Pedido/Pedido/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Consul;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Pedido
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConsulRegister();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+
+        public async Task<WriteResult> ConsulRegister()
+        {
+            var client = new ConsulClient(); // uses default host:port which is localhost:8500
+
+            var agentReg = new AgentServiceRegistration()
+            {
+                Address = "127.0.0.1",
+                ID = "api-pedido",
+                Name = "OrderService",
+                Port = 44349
+            };
+
+            return await client.Agent.ServiceRegister(agentReg);
+        }
+    }
+}
