cbaed55825a9da172bb272fa9faf128a44856ba6
lang-codes <lang.codes@gmail.com>
2020-03-24 12:03:13 -0300

    [lang-codes] Removing unused references.

--- src/ApiGateway/ApiGateway/Startup.cs
+++ src/ApiGateway/ApiGateway/Startup.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Consul;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using Ocelot.Provider.Consul;
 
 namespace ApiGateway
 {
@@ -29,11 +20,7 @@ namespace ApiGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //ConsulRegister();
-
             services.AddOcelot();
-                    //.AddConsul()
-                    //.AddConfigStoredInConsul();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
@@ -54,21 +41,6 @@ namespace ApiGateway
             app.UseHttpsRedirection();
             app.UseMvc();
             app.UseOcelot().Wait();
-        }
-
-        public async Task<WriteResult> ConsulRegister()
-        {
-            var client = new ConsulClient(); // uses default host:port which is localhost:8500
-
-            var agentReg = new AgentServiceRegistration()
-            {
-                Address = "127.0.0.1",
-                ID = "api-gateway",
-                Name = "ApiGateway",
-                Port = 44349
-            };
-
-            return await client.Agent.ServiceRegister(agentReg);
         }
     }
 }
